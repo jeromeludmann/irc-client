6ed6d843a9da8cc8ba63c6baea194d8e67239c9e
Jerome Ludmann <jeromeludmann@users.noreply.github.com>
2018-06-04 19:16:39 +0200

    Update project settings (#3)

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,60 @@
 const path = require("path");
 
-const shared = {
-  mode: "development",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-    alias: {
-      "@app": path.resolve(__dirname, "src"),
-    },
+const mode = "development";
+
+const resolve = {
+  extensions: [".ts", ".tsx", ".js"],
+  alias: {
+    "@app": path.resolve(__dirname, "src"),
   },
 };
 
-module.exports = [
-  {
-    ...shared,
-    target: "electron-main",
-    entry: "./src/main.ts",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "main.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: {
-                configFile: "../tsc/main.json",
-              },
-            },
-            "tslint-loader",
-          ],
+const rules = {
+  ts: ({ tsconfigPath }) => ({
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "ts-loader",
+        options: {
+          configFile: tsconfigPath,
         },
-      ],
-    },
-    node: {
-      __dirname: false,
-      __filename: false,
-    },
+      },
+      "tslint-loader",
+    ],
+  }),
+};
+
+const mainConfig = {
+  mode,
+  resolve,
+  target: "electron-main",
+  entry: "./src/main.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
-  {
-    ...shared,
-    target: "electron-renderer",
-    entry: "./src/renderer.tsx",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "renderer.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: {
-                configFile: "../tsc/renderer.json",
-              },
-            },
-            "tslint-loader",
-          ],
-        },
-      ],
-    },
+  module: {
+    rules: [rules.ts({ tsconfigPath: "../tsc/main.json" })],
   },
-];
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+};
+
+const rendererConfig = {
+  mode,
+  resolve,
+  target: "electron-renderer",
+  entry: "./src/renderer.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "renderer.js",
+  },
+  module: {
+    rules: [rules.ts({ tsconfigPath: "../tsc/renderer.json" })],
+  },
+};
+
+module.exports = [mainConfig, rendererConfig];
