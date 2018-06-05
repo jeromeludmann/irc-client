513e6663537af9693bc63755871d4c24a83fc6be
Jerome Ludmann <jeromeludmann@users.noreply.github.com>
2018-06-05 04:28:04 +0200

    Add memoized selectors (#4)

--- src/Channel/connected.ts
+++ src/Channel/connected.ts
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import { RootState } from "@app/reducers/rootReducer";
-import { getMessages } from "@app/Channel/selectors";
+import { getMessages, getMessagesCount } from "@app/Channel/selectors";
 import { Channel as Component } from "@app/Channel/component";
 
 interface OwnProps {
--- src/Channel/selectors.ts
+++ src/Channel/selectors.ts
@@ -1,6 +1,12 @@
+import { createSelector } from "reselect";
 import { RootState } from "@app/reducers/rootReducer";
 
 export const getMessages = (
   state: RootState,
   { server, channel }: { server: string; channel: string },
 ) => state.servers[server].channels[channel].messages;
+
+export const getMessagesCount = createSelector(
+  getMessages,
+  messages => (messages ? messages.length : 0),
+);
