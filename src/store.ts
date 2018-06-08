96bbee63c594e107edc42421fe7795f793e7c382
Jerome Ludmann <jerome.ludmann@gmail.com>
2018-06-08 18:25:39 +0200

    Add redux thunk middleware

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "@app/state";
 import mockedState from "@app/mockedState";
 
-export const store = createStore(rootReducer, mockedState);
+export const store = createStore(
+  rootReducer,
+  mockedState,
+  applyMiddleware(thunk),
+);
