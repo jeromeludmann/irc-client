cfb849dfde1e13a72f04a276d026d2f456e2dfb5
Jerome Ludmann <jerome.ludmann@gmail.com>
2018-06-11 23:31:16 +0200

    Remove thunks
    Update TypeScript to 3.0.0-dev.20180609

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,5 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import rootReducer from "@app/state";
 import mockedState from "@app/mockedState";
 
-export const store = createStore(
-  rootReducer,
-  mockedState,
-  applyMiddleware(thunk),
-);
+export const store = createStore(rootReducer, mockedState);
