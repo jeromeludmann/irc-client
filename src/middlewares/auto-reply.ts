9a40d4044bb78337fb28eb0ec72f761fd169ffa7
Jerome Ludmann <jerome.ludmann@gmail.com>
2018-06-22 15:12:12 +0200

    Enable strictFunctionTypes rule

--- src/middlewares/auto-reply.ts
+++ src/middlewares/auto-reply.ts
@@ -7,33 +7,33 @@ import { sendUser, sendNick, sendPong } from "@app/actions/message-out";
 import { IncomingServerPingAction, SERVER_PING } from "@app/actions/message-in";
 import { RootState } from "@app/reducers";
 
-type Actions = ConnectionEstablishedAction | IncomingServerPingAction;
+type AutoReplyAction = ConnectionEstablishedAction | IncomingServerPingAction;
+
+const registry: {
+  [action: string]: (action: AutoReplyAction, state: RootState) => Action[];
+} = {
+  [CONNECTION_ESTABLISHED](action, state) {
+    return [
+      sendUser(action.route.serverKey, state.user.user, state.user.real),
+      sendNick(action.route.serverKey, state.user.nick),
+    ];
+  },
+  [SERVER_PING](action) {
+    return [
+      sendPong(
+        action.route.serverKey,
+        (action as IncomingServerPingAction).payload.key,
+      ),
+    ];
+  },
+};
 
 export const autoReply: Middleware<{}, RootState> = store => next => (
-  action: Actions,
+  action: AutoReplyAction,
 ) => {
   next(action);
 
-  if (actions.hasOwnProperty(action.type)) {
-    actions[action.type](action, store.getState()).forEach(next);
+  if (registry.hasOwnProperty(action.type)) {
+    registry[action.type](action, store.getState()).forEach(next);
   }
-};
-
-const onConnectionEstablished = (
-  action: ConnectionEstablishedAction,
-  state: RootState,
-) => [
-  sendUser(action.route.serverKey, state.user.user, state.user.real),
-  sendNick(action.route.serverKey, state.user.nick),
-];
-
-const onServerPing = (action: IncomingServerPingAction) => [
-  sendPong(action.route.serverKey, action.payload.key),
-];
-
-const actions: {
-  [action: string]: (action: Actions, state: RootState) => Action[];
-} = {
-  [CONNECTION_ESTABLISHED]: onConnectionEstablished,
-  [SERVER_PING]: onServerPing,
 };
--- src/middlewares/command.ts
+++ src/middlewares/command.ts
@@ -8,6 +8,32 @@ import { RootState } from "@app/reducers";
 import { sendRaw, sendJoin } from "@app/actions/message-out";
 import { sendPrivmsg } from "@app/actions/message-out";
 
+const registry: {
+  [command: string]: (
+    { serverKey, params }: { serverKey: string; params: string },
+  ) => Action;
+} = {
+  connect({ params }) {
+    // TODO dispatch error action
+    if (!params) {
+      // tslint:disable-next-line
+      console.warn("params is missing");
+      return { type: "ACTION" };
+    }
+
+    return connectServer(params);
+  },
+  disconnect({ serverKey }) {
+    return disconnectServer(serverKey);
+  },
+  join({ serverKey, params: channel }) {
+    return sendJoin(serverKey, channel);
+  },
+  raw({ serverKey, params: message }) {
+    return sendRaw(serverKey, message);
+  },
+};
+
 export const commands: Middleware<{}, RootState> = store => next => (
   action: EnterInputValueAction,
 ) => {
@@ -41,38 +67,4 @@ export const commands: Middleware<{}, RootState> = store => next => (
   }
 
   next(registry[command]({ serverKey, params: regExpResult[3] }));
-};
-
-type Command = (
-  { serverKey, params }: { serverKey: string; params: string },
-) => Action;
-
-const connect: Command = ({ params }) => {
-  // TODO dispatch error action
-  if (!params) {
-    // tslint:disable-next-line
-    console.warn("params is missing");
-    return { type: "ACTION" };
-  }
-
-  return connectServer(params);
-};
-
-const disconnect: Command = ({ serverKey }) => {
-  return disconnectServer(serverKey);
-};
-
-const join: Command = ({ serverKey, params: channel }) => {
-  return sendJoin(serverKey, channel);
-};
-
-const raw: Command = ({ serverKey, params: message }) => {
-  return sendRaw(serverKey, message);
-};
-
-const registry: { [command: string]: Command } = {
-  connect,
-  disconnect,
-  join,
-  raw,
 };
