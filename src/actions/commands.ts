e1c8fbbd9757c4d5fa31816b0be1d30dae377352
Jerome Ludmann <jeromeludmann@users.noreply.github.com>
2018-07-22 20:47:28 +0200

    Add /help command (#24)
    * Add command descriptions
    * Add generic types to commands
    * Fix regular expressions

--- src/actions/commands.ts
+++ src/actions/commands.ts
@@ -4,33 +4,35 @@ import {
   disconnectServer,
   sendMessage,
   sendRaw,
+  DisconnectServerAction,
+  ConnectServerAction,
+  SendRawMessageAction,
 } from "@app/actions/socket";
 import { closeWindow } from "@app/actions/ui";
-import { Route } from "@app/Route";
+import { Route, RoutedAction, STATUS } from "@app/Route";
 
-export type Command = {
+export type Command<A = Action<string>> = {
+  description: string;
   syntax: string;
   regexp: RegExp;
-  callback: (route: Route, ...args: string[]) => Action<string>;
+  callback: (route: Route, ...args: string[]) => A;
 };
 
-/**
- * Command: /close
- *
- * Close current window.
- */
+type CommandRegistry = { [command: string]: Command };
+
+// close
+
 export const close: Command = {
-  syntax: "(no required args)",
+  description: "Closes the current window",
+  syntax: "",
   regexp: /\s*/,
   callback: route => closeWindow(route),
 };
 
-/**
- * Command: /connect
- *
- * Connect to a server.
- */
-export const connect: Command = {
+// connect
+
+export const connect: Command<ConnectServerAction> = {
+  description: "Connects to a server",
   syntax: "[-n] <host> [port]",
   regexp: /^(?:(-n)\s+)?(\S+)(?:\s+(\d{1,5})?)?$/,
   callback: (route, option, host, port?) =>
@@ -42,72 +44,114 @@ export const connect: Command = {
     ),
 };
 
-/**
- * Command: /disconnect
- *
- * Disconnect from a server.
- */
-export const disconnect: Command = {
+// disconnect
+
+export const disconnect: Command<DisconnectServerAction> = {
+  description: "Disconnects from the current server",
   syntax: "[quit message]",
-  regexp: /^(\S+)?$/,
+  regexp: /^(.*)$/,
   callback: (route, quitMessage?) =>
     disconnectServer(route.serverKey, quitMessage),
 };
 
-/**
- * Command: /join
- *
- * Join a channel.
- */
-export const join: Command = {
+// help
+
+export const PRINT_HELP_BY_DEFAULT = "COMMAND/PRINT_HELP_BY_DEFAULT";
+export const PRINT_HELP_ABOUT_COMMAND = "COMMAND/PRINT_HELP_ABOUT_COMMAND";
+
+export interface PrintHelpByDefaultAction
+  extends RoutedAction<typeof PRINT_HELP_BY_DEFAULT> {
+  payload: { commands: CommandRegistry };
+}
+
+export interface PrintHelpAboutCommandAction
+  extends RoutedAction<typeof PRINT_HELP_ABOUT_COMMAND> {
+  payload: { command: Command & { name: string } };
+}
+
+export const help: Command<
+  PrintHelpByDefaultAction | PrintHelpAboutCommandAction
+> = {
+  description: "Prints help about available commands",
+  syntax: "[command]",
+  regexp: /^(\S+)?$/,
+  callback: (route, commandName?) => {
+    if (commandName) {
+      commandName = commandName.toLowerCase();
+    }
+
+    if (commandName && registry.hasOwnProperty(commandName)) {
+      return {
+        type: PRINT_HELP_ABOUT_COMMAND,
+        payload: { command: { ...registry[commandName], name: commandName } },
+        route: { ...route, channelKey: STATUS },
+      };
+    }
+
+    return {
+      type: PRINT_HELP_BY_DEFAULT,
+      payload: { commands: registry },
+      route: { ...route, channelKey: STATUS },
+    };
+  },
+};
+
+// join
+
+export const join: Command<SendRawMessageAction> = {
+  description: "Joins a channel",
   syntax: "<channel>",
-  regexp: /^([&|#|\+|!]\S+)$/,
+  regexp: /^([&|#|\+|!]\S+)$/, // TODO extract (see Route.ts)
   callback: (route, channel) => sendMessage(route.serverKey, "join", channel),
 };
 
-/**
- * Command: /msg
- *
- * Send a message to a channel or a nickname.
- */
-export const msg: Command = {
+// msg
+
+export const msg: Command<SendRawMessageAction> = {
+  description: "Sends a message to a channel or a nick",
   syntax: "<channel or nick> <text>",
   regexp: /^(\S+)\s+(.+)$/,
   callback: (route, channel, text) =>
     sendMessage(route.serverKey, "privmsg", channel, text),
 };
 
-/**
- * Command: /part
- *
- * Part from a channel.
- */
-export const part: Command = {
+// part
+
+export const part: Command<SendRawMessageAction> = {
+  description: "Leaves the channel",
   syntax: "<channel> [text]",
   regexp: /^(\S+)(?:\s+(.+))?$/,
   callback: (route, channel, text) =>
     sendMessage(route.serverKey, "part", channel, text),
 };
 
-/**
- * Command: /quit
- *
- * Quit a server.
- */
-export const quit: Command = {
-  syntax: "<quit message>",
+// quit
+
+export const quit: Command<SendRawMessageAction> = {
+  description: "Disconnects from the current server",
+  syntax: "[quit message]",
   regexp: /^(.*)$/,
   callback: (route, text) => sendMessage(route.serverKey, "quit", text),
 };
 
-/**
- * Command: /raw
- * Command: /quote
- *
- * Send a raw message.
- */
-export const raw: Command = {
+// raw (quote)
+
+export const raw: Command<SendRawMessageAction> = {
+  description: "Sends a raw message",
   syntax: "<raw message>",
   regexp: /^(.+)$/,
   callback: (route, message) => sendRaw(route.serverKey, message),
+};
+
+export const registry: CommandRegistry = {
+  close,
+  connect,
+  disconnect,
+  help,
+  join,
+  msg,
+  part,
+  quit,
+  quote: raw,
+  raw,
 };
--- src/middlewares/commands.ts
+++ src/middlewares/commands.ts
@@ -2,16 +2,7 @@ import { Middleware } from "redux";
 import { isStatus, isRaw } from "@app/Route";
 import { AppState } from "@app/reducers";
 import { EnterInputValueAction, ENTER_INPUT_VALUE } from "@app/actions/ui";
-import {
-  Command,
-  close,
-  connect,
-  disconnect,
-  join,
-  msg,
-  part,
-  raw,
-} from "@app/actions/commands";
+import { registry, help, msg } from "@app/actions/commands";
 
 /**
  * Commands Handler Middleware
@@ -46,26 +37,13 @@ export const commands: Middleware<{}, AppState> = store => next => (
   const commandName = parsedCommand[1].toLowerCase();
   const commandArgs = parsedCommand[2] || "";
 
-  if (!commandsMap.hasOwnProperty(commandName)) {
-    // TODO help() action
-    // tslint:disable-next-line
-    console.warn("Command not found: " + commandName);
+  if (!registry.hasOwnProperty(commandName)) {
+    next(help.callback(route));
     return;
   }
 
-  const command = commandsMap[commandName];
+  const command = registry[commandName];
   const parsedArgs = commandArgs.match(command.regexp) || [];
 
   next(command.callback(route, ...parsedArgs.slice(1)));
-};
-
-const commandsMap: { [command: string]: Command } = {
-  close,
-  connect,
-  disconnect,
-  join,
-  msg,
-  part,
-  quote: raw,
-  raw,
 };
