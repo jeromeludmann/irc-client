5aa88514cd40ef9bbad342a3225bb5f5a9d771c0
Jerome Ludmann <jerome.ludmann@gmail.com>
2018-08-21 21:42:30 +0200

    Update servers reducer test

--- src/reducers/__tests__/servers.test.ts
+++ src/reducers/__tests__/servers.test.ts
@@ -2,10 +2,10 @@ import { reduceServers, serversInitialState } from "@app/reducers/servers";
 import { closeWindow } from "@app/actions/ui";
 import { STATUS } from "@app/Route";
 import { serverInitialState } from "@app/reducers/server";
-import { LOOKUP_SUCCESS, lookup } from "@app/actions/socket";
+import { lookup } from "@app/actions/socket";
 
-describe("servers reducer", () => {
-  test("initializing without state", () => {
+describe("reduce servers", () => {
+  test("without state", () => {
     expect(
       reduceServers(
         undefined,
@@ -15,58 +15,44 @@ describe("servers reducer", () => {
     ).toMatchSnapshot();
   });
 
-  test("initializing with state", () => {
-    expect(
-      reduceServers(
-        serversInitialState,
-        { type: "", route: { serverKey: "server1", channelKey: "" } },
-        { route: { serverKey: "server1", channelKey: STATUS } },
-      ),
-    ).toMatchSnapshot();
+  describe("by dispatching close window", () => {
+    test("that is not a status window", () => {
+      expect(
+        reduceServers(
+          serversInitialState,
+          closeWindow({ serverKey: "server1", channelKey: "#channel" }),
+          { route: { serverKey: "server1", channelKey: "#channel" } },
+        ),
+      );
+    });
+
+    describe("that is a status window", () => {
+      test("without server", () => {
+        expect(
+          reduceServers(
+            serversInitialState,
+            closeWindow({ serverKey: "server1", channelKey: STATUS }),
+            { route: { serverKey: "server1", channelKey: STATUS } },
+          ),
+        );
+      });
+
+      test("with two servers", () => {
+        expect(
+          reduceServers(
+            {
+              server1: serverInitialState,
+              server2: serverInitialState,
+            },
+            closeWindow({ serverKey: "server1", channelKey: STATUS }),
+            { route: { serverKey: "server1", channelKey: STATUS } },
+          ),
+        );
+      });
+    });
   });
 
-  describe("close window", () => {
-    test("without server", () => {
-      expect(
-        reduceServers(
-          serversInitialState,
-          closeWindow({ serverKey: "server1", channelKey: STATUS }),
-          { route: { serverKey: "server1", channelKey: STATUS } },
-        ),
-      );
-    });
-
-    test("close non status window, with two servers", () => {
-      expect(
-        reduceServers(
-          serversInitialState,
-          closeWindow({ serverKey: "server1", channelKey: STATUS }),
-          { route: { serverKey: "server1", channelKey: STATUS } },
-        ),
-      );
-    });
-
-    test("close status window, with two servers", () => {
-      expect(
-        reduceServers(
-          serversInitialState,
-          closeWindow({ serverKey: "server1", channelKey: STATUS }),
-          { route: { serverKey: "server1", channelKey: STATUS } },
-        ),
-      );
-
-      expect(
-        reduceServers(
-          {
-            server1: serverInitialState,
-            server2: serverInitialState,
-          },
-          closeWindow({ serverKey: "server1", channelKey: STATUS }),
-          { route: { serverKey: "server1", channelKey: STATUS } },
-        ),
-      );
-    });
-
+  describe("by dispatching lookup", () => {
     expect(
       reduceServers(
         { server1: serverInitialState },
