9c359b61a6695dd7408240bed1156488a0cca928
Jerome Ludmann <jerome.ludmann@gmail.com>
2018-08-25 15:32:32 +0200

    Add channel activity reducer test

---
+++ src/reducers/channel/__tests__/activity.test.ts
@@ -0,0 +1,38 @@
+import { reduceActivity } from "@app/reducers/channel/activity";
+import { messageCallbacks } from "@app/actions/messages";
+import { switchWindow } from "@app/actions/ui";
+
+describe("reduce channel activity", () => {
+  const extraParams = {
+    user: { nick: "nick", user: "user", real: "name" },
+    route: { serverKey: "server1", channelKey: "#channel" },
+  };
+
+  test("by joining channel", () => {
+    expect(
+      reduceActivity(
+        undefined,
+        messageCallbacks["JOIN"](
+          "server1",
+          {
+            nick: "nick",
+            user: "user",
+            host: "host",
+          },
+          ["#channel"],
+        ),
+        extraParams,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  test("by switching window", () => {
+    expect(
+      reduceActivity(
+        undefined,
+        switchWindow({ serverKey: "server1", channelKey: "#channel" }),
+        extraParams,
+      ),
+    ).toMatchSnapshot();
+  });
+});
