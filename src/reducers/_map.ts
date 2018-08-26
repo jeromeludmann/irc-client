1df091e9b63758ef148c2146859bd62434fea414
Jerome Ludmann <jerome.ludmann@gmail.com>
2018-08-26 19:24:56 +0200

    Remove unwanted complexity of generic map()

--- src/reducers/_map.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from "redux";
-
-export const mapReducer = <S, E = void>(map: {
-  [action: string]: (state: S, action: Action, extraStates?: E) => S;
-}) => (state: S, action: Action, extraStates?: E): S =>
-  map.hasOwnProperty(action.type)
-    ? map[action.type](state, action, extraStates)
-    : state;
